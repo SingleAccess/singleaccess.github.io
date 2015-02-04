add98b33bd227f1a5163c97f1800b40a169226b1
Dominic Ho <domtkho@gmail.com>
2015-02-04 20:25:42 +0800

    Added mail chimp

---
+++ main.js
@@ -0,0 +1,11 @@
+(function($) {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+
+  fnames[1]='NAME';
+  ftypes[1]='text';
+  fnames[0]='EMAIL';
+  ftypes[0]='email';
+}(jQuery));
+
+var $mcj = jQuery.noConflict(true);
