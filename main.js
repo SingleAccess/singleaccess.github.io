0f4afb34b4271100c6aaa0d6406d50b80363fd8a
Dominic Ho <domtkho@gmail.com>
2015-02-04 22:41:48 +0800

    Added Ajax.

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-(function($) {
-  window.fnames = new Array();
-  window.ftypes = new Array();
+$(document).ready(function () {
 
-  fnames[1]='NAME';
-  ftypes[1]='text';
-  fnames[0]='EMAIL';
-  ftypes[0]='email';
-}(jQuery));
+  var $form = $('#mc-embedded-subscribe-form');
 
-var $mcj = jQuery.noConflict(true);
+  $('#mc-embedded-subscribe').on('click', function(event) {
+    event.preventDefault();
+    register($form);
+
+  function register($form) {
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache       : false,
+      dataType    : 'jsonp',
+      jsonp: "c",
+      // crossDomain: true,
+      contentType: "application/json; charset=utf-8",
+      error       : function(err) { $('#notification_container').html('<span class="alert">FUCK YOU MAILCHIMP</span>'); },
+      success     : function(data) {
+        console.log(data);
+        if (data.result === "success") {
+          $('#notification_container').html('<span class="success">'+"SUCCESSSSSS"+'</span>');
+
+        }
+        // if (data.result != "success") {
+        //   var message = data.msg.substring(4);
+        //   $('#notification_container').html('<span class="alert">'+HELLPPPPPPPPPPPPPPPP+'</span>');
+        // } else {
+        //   var message = data.msg;
+
+        //   $('#notification_container').html('<span class="success">'+message+'</span>');
+        // }
+      }
+    });
+  }
+
+  });
+});
